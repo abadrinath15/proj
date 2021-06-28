5173678bfd9af78415c2ccb7adf89dc02a1a4f6d
Aditya Badrinath <abadrinath15@gmail.com>
2021-06-27 20:42:32 -0400

    I guess we'll try this...

---
+++ proj/app.py
@@ -0,0 +1,30 @@
+"""This is the application module.
+"""
+import dash
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import distinct, select
+from apps.summaries import generate_summary_layout
+from callbacks import register_callbacks
+from db_structure import build_bond
+
+# Create the dash app
+server = Flask(__name__)
+app = dash.Dash(name=__name__, server=server, suppress_callback_exceptions=True)
+app.server.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+# Connection for a local postgres test table
+app.server.config[
+    "SQLALCHEMY_DATABASE_URI"
+] = "postgresql://aditya:postgres@localhost/bond_universe"
+
+db = SQLAlchemy(app.server)
+Bond = build_bond(db)
+# Dates are independent of user input, so we'll go ahead and get that done now
+stmt = select(distinct(Bond.eff_date))
+result = db.session.execute(stmt)
+app.layout = generate_summary_layout(sorted([x[0] for x in result]))
+register_callbacks(app, db, Bond)
+
+if __name__ == "__main__":
+    app.run_server(debug=True)
